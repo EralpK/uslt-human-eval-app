f273f44b1bc8f8563e503931e648313a31107393
EralpK <43544065+EralpK@users.noreply.github.com>
2024-11-07 23:02:47 +0300

    added instructions regarding the app

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
 
-st.write("## Welcome to the Text Pair Evaluation Tool")
+#st.write("## Welcome to the Text Pair Evaluation Tool")
+st.write("## Human Evaluation Tool for Legal Text Simplification")
 
-st.write(" Use the sidebar to navigate to the Instructions or Evaluation page.")
+st.write(" Use the sidebar to navigate to the Instructions or the Evaluation page.")
 
-st.write("### Additional info for Aykut Hoca")
-st.write(
-    "Hocam şuanda totalde 50 tane text pair var, 25 tane original (non-simplified) text var elimizde. Bu 25 tane original text için bizim model ve baseline modeller arasından en iyisinin elde ettiği simplificationlar var. Bunlarında sırası karışık, art arda aynı original text her zaman gelemeyebilir. Diğer detaylar için sidebar'dan Instructions sayfasına gidebilirsiniz."
-)
+st.warning(" Make sure that you carefully read and understand the guidelines presented in the Instructions page before starting to evaluate text pairs in the Evaluation page.")
+
+#st.write("### Additional info for Aykut Hoca")
+#st.write(
+#    "Hocam şuanda totalde 50 tane text pair var, 25 tane original (non-simplified) text var elimizde. Bu 25 tane original text için bizim model ve baseline modeller arasından en iyisinin elde ettiği simplificationlar var. Bunlarında sırası karışık, art arda aynı original text her zaman gelemeyebilir. Diğer detaylar için sidebar'dan Instructions sayfasına gidebilirsiniz."
+#)
+
